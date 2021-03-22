3d581078f26d4890e365dbd6b0d704c5a3ee612c
Harshil Kapoor <harshilkapoor@gmail.com>
2021-03-22 16:26:32 +0530

    Add .NET project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Controllers
+{
+    [Route("Home")]
+    public class HomeController
+    {
+        [HttpGet("Index")]
+        public string Index()
+        {
+            return "Index action";
+        }
+    }
+}
