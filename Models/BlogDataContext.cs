eb4b0617856ea253322d4bccad365e1c20be8f55
Harshil Kapoor <harshilkapoor@gmail.com>
2021-03-31 22:29:28 +0530

    Add view component to view monthly specials - Add model, view to be rendered, use vc tag helper.

--- Models/BlogDataContext.cs
+++ Models/BlogDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ExploreCalifornia.ViewComponents;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -10,6 +11,44 @@ namespace ExploreCalifornia.Models
     public class BlogDataContext : DbContext
     {
         public DbSet<Post> Posts { get; set; }
+
+        public IQueryable<MonthlySpecial> MonthlySpecials
+        {
+            get
+            {
+                return new[]
+                {
+                    new MonthlySpecial
+                    {
+                        Key = "calm",
+                        Name = "California Calm",
+                        Type = "Day Spa Package",
+                        Price = 250,
+                    },
+                    new MonthlySpecial
+                    {
+                        Key = "desert",
+                        Name = "From desert to sea",
+                        Type = "2 Day Salton Sea",
+                        Price = 350,
+                    },
+                    new MonthlySpecial
+                    {
+                        Key = "backpack",
+                        Name = "Backpack Cali",
+                        Type = "Big Sur Retreat",
+                        Price = 620,
+                    },
+                    new MonthlySpecial
+                    {
+                        Key = "taste",
+                        Name = "Taste of California",
+                        Type = "Tapas & Groves",
+                        Price = 150,
+                    }
+                }.AsQueryable();
+            }
+        }
 
         public BlogDataContext([NotNullAttribute] DbContextOptions options)
             : base(options)
